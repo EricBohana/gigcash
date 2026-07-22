0bb324910ad240d3774b51a61c5876ed90ccf0a1
EricBohana <ericbohana10@gmail.com>
2026-07-21 23:34:01 -0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const metaEnv = (import.meta as any).env || {};
 
@@ -27,4 +28,4 @@ const safeConfig = hasFirebaseConfig ? firebaseConfig : {
 
 const app = initializeApp(safeConfig);
 export const auth = getAuth(app);
-
+export const db = getFirestore(app);
